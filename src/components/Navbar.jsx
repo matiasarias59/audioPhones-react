6606f2e2e5ed283d55e893385dca528b27015587
Matias Arias <matiasarias59@gmail.com>
2023-02-25 16:07:59 -0300

    Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 //import './Navbar.css'
 import SearchBar from './SearchBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
 
@@ -22,19 +22,19 @@ export default function Navbar() {
 
   }, [catalogue])
 
-/*   const HandleMenuName = e => {
-    e.stopPropagation();
-    console.log(e)
-    e.target.nextSibling.classList.toggle("active")
-  }
-
-
-  const HandleMenuList = e => {
-    e.stopPropagation();
-    console.log(e)
-    e.target.classList.remove("active")
-  }
- */
+  /*   const HandleMenuName = e => {
+      e.stopPropagation();
+      console.log(e)
+      e.target.nextSibling.classList.toggle("active")
+    }
+  
+  
+    const HandleMenuList = e => {
+      e.stopPropagation();
+      console.log(e)
+      e.target.classList.remove("active")
+    }
+   */
   /* *********************************************** */
 
   const [menuIsActive, setMenuIsActive] = useState(false);
@@ -58,17 +58,20 @@ export default function Navbar() {
 
     <nav className='navbar'>
       <div className={`navbar__icon`} onClick={handleOnClick}>
-        <FontAwesomeIcon icon={menuIsActive? faXmark : faBars} color="white" size='2x' />
+        <FontAwesomeIcon icon={menuIsActive ? faXmark : faBars} color="white" size='2x' />
       </div>
       <div className="navbar__logo__container">
         <Logo />
       </div>
-      <div className={`menu__container${menuIsActive?"--active":""}`}>
+      <div className={`menu__container${menuIsActive ? "--active" : ""}`}>
 
         <ul className='menu'>
           <li>
             <div className='menu__name' onClick={handleOnClickCategory} >
-              CATEGORIAS ▼
+              CATEGORIAS
+              <div className={`menu__name__icon${categoryMenuIsActive ? "--active" : ""}`} onClick={handleOnClickCategory}>
+                <FontAwesomeIcon icon={faCaretRight} color="white" size='1x' />
+              </div>
             </div>
             <ul className={`menu__list${categoryMenuIsActive ? "--active" : ""}`} onMouseLeave={handleOnClickCategory} >
               {categoryList.map((el, i) => {
@@ -81,7 +84,10 @@ export default function Navbar() {
           </li>
           <li>
             <div className='menu__name' onClick={handleOnClickBrand}>
-              MARCAS ▼
+              MARCAS 
+              <div className={`menu__name__icon${brandMenuIsActive ? "--active" : ""}`} onClick={handleOnClickBrand}>
+                <FontAwesomeIcon icon={faCaretRight} color="white" size='1x' />
+              </div>
             </div>
             <ul className={`menu__list${brandMenuIsActive ? "--active" : ""}`} onMouseLeave={handleOnClickBrand}>
               {brandList.map((el, i) => {
